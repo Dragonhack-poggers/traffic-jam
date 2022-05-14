9b7d3724b28618e28594327c5582de45664da80a
Amadej Senk <guide5ever@gmail.com>
2022-05-14 14:21:52 +0200

    Additional setup

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,17 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import express from 'express';
+import swaggerOptions from './config';
+import router from './routes';
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+export const app = express();
 
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/api', router);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+const PORT = 3000;
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+expressJSDocSwagger(app)(swaggerOptions);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+app.listen(PORT);
 
-module.exports = app;
