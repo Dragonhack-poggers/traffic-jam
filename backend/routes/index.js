eb4e8b5c49e9e804626e2e9d9ab85d2906cde8ae
Amadej Senk <guide5ever@gmail.com>
2022-05-14 14:38:46 +0200

    Finished setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,5 @@
 const router = express.Router({});
 
-router.use('/closure', closureRouter);
-router.use('/cover/minimal', minCoverRouter);
-router.use('/cover/canonical', canCoverRouter);
-router.use('/primary-key', primKeyRouter);
-router.use('/foreign-key', forKeyRouter);
-router.use('/normalization', normalizationRouter);
-router.use('/convert-to-sql', convSQLRouter);
-router.use('/check', checkRouter);
+router.use('/event', databaseRouter);
 
 export default router;
