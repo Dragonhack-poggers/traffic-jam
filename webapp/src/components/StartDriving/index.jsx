642d1cb805e9bd2933a74c3080cd9fa4885839e7
Martin Strekelj <martin.strekelj@easy.bi>
2022-05-14 15:31:34 +0200

    feat: add bluetooth subscription logic

---
+++ webapp/src/components/StartDriving/index.jsx
@@ -0,0 +1,61 @@
+import { IconButton, Flex } from "@chakra-ui/react";
+import { PhoneIcon, AtSignIcon } from "@chakra-ui/icons";
+import React, { useState, useEffect } from "react";
+import { notifyForTrafficJam } from "../../api/backend-api";
+
+const NEARBY_DEVICES_LIMIT = 2;
+const THIRTY_SECONDS = 30 * 1000;
+
+const StartDriving = () => {
+  const [isDriving, setDriving] = useState(false);
+  const [nearbyDevices, setNearbyDevices] = useState(0);
+  const toggleDriving = () => setDriving(!isDriving);
+
+  const iconProps = { w: 6, h: 6 };
+
+  useEffect(() => {
+    const fetchNearbyBluetoothDevices = async () => {
+      console.log("triggered");
+      const devices = await navigator.bluetooth.getDevices();
+      if (!devices) {
+        return setNearbyDevices(0);
+      }
+      setNearbyDevices(devices.length);
+    };
+
+    const subscription = setInterval(
+      isDriving ? fetchNearbyBluetoothDevices : () => {},
+      THIRTY_SECONDS
+    );
+    return () => clearInterval(subscription);
+  }, [isDriving]);
+
+  useEffect(() => {
+    const triggerTrafficJamEvent = async () => {
+      await notifyForTrafficJam();
+    };
+
+    if (nearbyDevices >= NEARBY_DEVICES_LIMIT) {
+      triggerTrafficJamEvent();
+    }
+  }, [nearbyDevices]);
+
+  return (
+    <Flex w='100%' alignItems={"center"} justifyContent='center' mt={8}>
+      <IconButton
+        onClick={toggleDriving}
+        p={12}
+        icon={
+          isDriving ? (
+            <PhoneIcon {...iconProps} />
+          ) : (
+            <AtSignIcon {...iconProps} />
+          )
+        }
+        isRound
+      />
+    </Flex>
+  );
+};
+
+export default StartDriving;
