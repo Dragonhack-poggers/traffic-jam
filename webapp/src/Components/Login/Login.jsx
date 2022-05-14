c4a96d9a733c9e58d5e79329843e33d16e089099
Martin Strekelj <martin.strekelj@easy.bi>
2022-05-14 23:20:23 +0200

    feat: add login and auth hook

--- webapp/src/Components/Login/Login.jsx
+++ webapp/src/Components/Login/Login.jsx
@@ -11,84 +11,132 @@ import {
   Link,
   chakra,
   Stack,
-  Container,
-} from "@chakra-ui/react"
-import { useState } from "react"
-import { FaUserAlt, FaLock } from "react-icons/fa"
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+import { loginUser } from "../../api/auth-api";
+import { useAppContext } from "../../lib/AppContext";
 
-const CFaUserAlt = chakra(FaUserAlt)
-const CFaLock = chakra(FaLock)
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
 
-const Login = (props) => {
-  const [showPassword, setShowPassword] = useState(false)
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, setUser } = useAppContext();
+  const toast = useToast();
 
-  const handleShowClick = () => setShowPassword(!showPassword)
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (!email.length || !password.length) {
+      toast({
+        variant: "solid",
+        status: "error",
+        title: "Error",
+        description: "Fields must not be empty",
+      });
+      return;
+    }
+
+    const data = { email, password };
+    try {
+      const user = await loginUser(data);
+      setUser(user);
+      toast({
+        variant: "solid",
+        status: "success",
+        title: "Success",
+        description: "Success",
+      });
+    } catch (error) {
+      console.error(error);
+      toast({
+        variant: "solid",
+        status: "error",
+        title: "Error",
+        description: "Something went wrong",
+      });
+    }
+  };
+
+  if (user) {
+    return (
+      <Link href='dashboard'>
+        <Button my={4}>Enter dashboard</Button>
+      </Link>
+    );
+  }
+
   return (
     <Flex
       flexDirection={"column"}
       justifyContent={"center"}
       alignItems={"center"}
-      my={4}
-    >
-      <form>
-        <Box>
-          <Stack
-            spacing={4}
-            p="1rem"
-            backgroundColor="whiteAlpha.900"
-            boxShadow="md"
-            borderRadius="md"
-          >
-            <FormControl>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents="none"
-                  children={<CFaUserAlt color="gray.300" />}
-                />
-                <Input type="email" placeholder="email address" />
-              </InputGroup>
-            </FormControl>
-            <FormControl>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents="none"
-                  color="gray.300"
-                  children={<CFaLock color="gray.300" />}
-                />
-                <Input
-                  type={showPassword ? "text" : "password"}
-                  placeholder="Password"
-                />
-                <InputRightElement width="4.5rem">
-                  <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                    {showPassword ? "Hide" : "Show"}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-              <FormHelperText textAlign="right">
-                <Link>forgot password?</Link>
-              </FormHelperText>
-            </FormControl>
-            <Button
-              borderRadius="md"
-              type="submit"
-              variant="solid"
-              colorScheme="teal"
-              width="full"
-            >
-              Login
-            </Button>
-          </Stack>
-        </Box>
-      </form>
+      my={4}>
+      <Box>
+        <Stack
+          spacing={4}
+          p='1rem'
+          backgroundColor='whiteAlpha.900'
+          boxShadow='md'
+          borderRadius='md'>
+          <FormControl>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents='none'
+                children={<CFaUserAlt color='gray.300' />}
+              />
+              <Input
+                type='email'
+                placeholder='email address'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </InputGroup>
+          </FormControl>
+          <FormControl>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents='none'
+                color='gray.300'
+                children={<CFaLock color='gray.300' />}
+              />
+              <Input
+                type={showPassword ? "text" : "password"}
+                placeholder='Password'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <InputRightElement width='4.5rem'>
+                <Button h='1.75rem' size='sm' onClick={handleShowClick}>
+                  {showPassword ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <FormHelperText textAlign='right'>
+              <Link>forgot password?</Link>
+            </FormHelperText>
+          </FormControl>
+          <Button
+            onClick={onSubmit}
+            borderRadius='md'
+            type='submit'
+            variant='solid'
+            colorScheme='teal'
+            width='full'>
+            Login
+          </Button>
+        </Stack>
+      </Box>
       <Box>
         Not registered yet?{" "}
-        <Link color="teal.500" href="#">
+        <Link color='teal.500' href='/register'>
           Sign Up
         </Link>
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- webapp/src/pages/LoginPage.jsx
+++ webapp/src/pages/LoginPage.jsx
@@ -1,24 +1,26 @@
-import { Flex } from "@chakra-ui/react"
-import React from "react"
-import StartDriving from "../components/StartDriving"
-import Login from "../Components/Login/Login"
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
+import StartDriving from "../components/StartDriving";
+import Login from "../Components/Login/Login";
 
 const LoginPage = () => {
   return (
-    <Flex
-      flexDirection="column"
-      width="100wh"
-      height="100vh"
-      backgroundColor="gray.200"
-      justifyContent="center"
-      alignItems="center"
-    >
-      {/* <Container maxW={"container.xl"}> */}
+    <Box backgroundColor='gray.200'>
       <StartDriving />
-      <Login />
-      {/* </Container> */}
-    </Flex>
-  )
-}
+      <Flex
+        flexDirection='column'
+        width='100wh'
+        minHeight='100vh'
+        backgroundColor='gray.200'
+        justifyContent='center'
+        alignItems='center'>
+        {/* <Container maxW={"container.xl"}> */}
 
-export default LoginPage
+        <Login />
+        {/* </Container> */}
+      </Flex>
+    </Box>
+  );
+};
+
+export default LoginPage;
