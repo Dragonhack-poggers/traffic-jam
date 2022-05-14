fff67a449bcfbc016c9348f61dafe844309bce31
ltomazic <62839996+ltomazic@users.noreply.github.com>
2022-05-14 19:27:14 +0200

    database insert

--- backend/database/databaseConnection.js
+++ backend/database/databaseConnection.js
@@ -1,10 +1,14 @@
 const supabaseImport = require("@supabase/supabase-js")
 
 const publicAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3amx2anZqcXppbGRrdmx0emljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTI1MjEzNjIsImV4cCI6MTk2ODA5NzM2Mn0.hyMIJ0kfY3fd6jJ207bDRj7BaMpRo_5eeTTXZBEXRMw"
-const supabase = supabaseImport.createClient("https://xyzcompany.supabase.co", publicAnonKey)
+const supabase = supabaseImport.createClient("https://vwjlvjvjqzildkvltzic.supabase.co", publicAnonKey)
 
-console.log(supabase)
+async function insertDatabase (long, lat, numberOfDevices) {
+  const { data, error } = await supabase
+    .from('traffic-jam')
+    .insert([
+      { long: 1.0, lat: 0.1, numberOfDevices: 400 }
+    ])
+}
 
-module.exports = supabase;
-
-
+module.exports = insertDatabase;
