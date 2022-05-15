62e8ef2df0ba6cdad1a693d0381194c1868a998c
Amadej Senk <guide5ever@gmail.com>
2022-05-15 02:04:20 +0200

    Added upsertUser endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,29 @@
 const express = require('express');
-const { insertEvent, getEvents } = require('../backend/database/databaseConnection')
+const { insertEvent, getEvents, upsertUser } = require('../backend/database/databaseConnection')
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+// Add event
 app.post('/event', async (req, resp) => {
   const { body } = req;
   const dbResponse = await insertEvent(body.long, body.lat, body.numberOfDevices);
   resp.send('Success');
 });
 
+// Get all events
 app.get('/event', async (req, resp) => {
   const response = await getEvents();
   resp.send(response);
+});
+
+// Log time to DB
+app.post('/update-user', async (req, resp) => {
+  const { body } = req;
+  const dbResponse = await upsertUser(body.userId, body.time);
+  resp.send('Success');
 });
 
 const PORT = 3000;
